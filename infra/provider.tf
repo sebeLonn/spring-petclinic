b4ebedf8808293d7798b07d0ac82712e5acc8b4e
Talha Keles <kelesstalha@gmail.com>
2026-08-12 21:49:53 +0300

    Migrate Terraform state to S3 remote backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "petclinic-terraform-state-597765856364"
+    key            = "infra/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "petclinic-terraform-locks"
+    encrypt        = true
+    profile        = "dev-mfa"
+  }
 }
 
 provider "aws" {
